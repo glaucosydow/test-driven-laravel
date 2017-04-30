3e4d9314043a649202c1407b04ebb62d2d378f32
Yoyosan <veselu@gmail.com>
2017-04-30 15:03:22 +0300

    Get rid of the database dependency.

--- tests/Unit/ReservationTest.php
+++ tests/Unit/ReservationTest.php
@@ -5,18 +5,18 @@ namespace Tests\Unit;
 use App\Concert;
 use Tests\TestCase;
 use App\Reservation;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Database\Eloquent\Collection;
 
 class ReservationTest extends TestCase
 {
-	use DatabaseMigrations;
-
     /** @test */
     public function calculating_the_total_cost()
     {
-        $concert = factory(Concert::class)->create(['ticket_price' => 1200]);
-        $concert->addTickets(3);
-        $tickets = $concert->findTickets(3);
+        $tickets = new Collection([
+        	(object) ['price' => 1200],
+        	(object) ['price' => 1200],
+        	(object) ['price' => 1200],
+    	]);
 
         $reservation = new Reservation($tickets);
 
